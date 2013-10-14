f361fa894527a85735dc0493165842354decfaf2
Michael Woolnough <michael.woolnough@gmail.com>
2013-10-14 11:02:11 +0100

    Added other io methods (requires fully testing)

--- memio.go
+++ memio.go
@@ -22,7 +22,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// MemIO implements Read, Write, Seek and Close methods for a byte slice.
+// MemIO implements Read, Write, Seek, Close and other io methods for a byte slice.
 package memio
 
 import "io"
@@ -30,7 +30,7 @@ import "io"
 // Closed is an error returned when trying to perform an operation after using Close().
 type Closed struct{}
 
-func (_ Closed) Error() string {
+func (Closed) Error() string {
 	return "operation not permitted when closed"
 }
 
@@ -39,7 +39,7 @@ type readMem struct {
 	pos  int
 }
 
-// Use a byte slice for reading. Implements io.Reader, io.Seeker and io.Closer.
+// Use a byte slice for reading. Implements io.Reader, io.Seeker, io.Closer, io.ReaderAt, io.ByteReader and io.WriterTo.
 func Open(data []byte) *readMem {
 	return &readMem{data, 0}
 }
@@ -53,6 +53,17 @@ func (b *readMem) Read(p []byte) (int, error) {
 	n := copy(p, b.data[b.pos:])
 	b.pos += n
 	return n, nil
+}
+
+func (b *readMem) ReadByte() (byte, error) {
+	if b.data == nil {
+		return 0, &Closed{}
+	} else if b.pos >= len(b.data) {
+		return 0, io.EOF
+	}
+	c := b.data[b.pos]
+	b.pos++
+	return c, nil
 }
 
 func (b *readMem) Seek(offset int64, whence int) (int64, error) {
@@ -78,12 +89,31 @@ func (b *readMem) Close() error {
 	return nil
 }
 
+func (b *readMem) ReadAt(p []byte, off int64) (int, error) {
+	if b.data == nil {
+		return 0, &Closed{}
+	} else if off >= int64(len(b.data)) {
+		return 0, io.EOF
+	}
+	return copy(p, b.data[off:]), nil
+}
+
+func (b *readMem) WriteTo(f io.Writer) (int64, error) {
+	if b.data == nil {
+		return 0, &Closed{}
+	} else if b.pos >= len(b.data) {
+		return 0, io.EOF
+	}
+	n, err := f.Write(b.data[b.pos:])
+	return int64(n), err
+}
+
 type writeMem struct {
 	data *[]byte
 	pos  int
 }
 
-// Use a byte slice for writing. Implements io.Writer, io.Seeker and io.Closer.
+// Use a byte slice for writing. Implements io.Writer, io.Seeker, io.Closer, io.WriterAt, io.ByteWriter and io.ReaderFrom.
 func Create(data *[]byte) *writeMem {
 	return &writeMem{data, 0}
 }
@@ -92,23 +122,55 @@ func (b *writeMem) Write(p []byte) (int, error) {
 	if b.data == nil {
 		return 0, &Closed{}
 	}
-	if end := b.pos + len(p); end > len(*b.data) {
-		if end < cap(*b.data) {
-			*b.data = (*b.data)[:end]
-		} else {
-			var newData []byte
-			if len(*b.data) < 512 {
-				newData = make([]byte, end, end<<1)
-			} else {
-				newData = make([]byte, end, end+(end>>2))
-			}
-			copy(newData, *b.data)
-			*b.data = newData
-		}
-	}
+	b.setSize(b.pos + len(p))
 	n := copy((*b.data)[b.pos:], p)
 	b.pos += n
 	return n, nil
+}
+
+func (b *writeMem) WriteAt(p []byte, off int64) (int, error) {
+	if b.data == nil {
+		return 0, &Closed{}
+	}
+	b.setSize(int(off) + len(p))
+	return copy((*b.data)[off:], p), nil
+}
+
+func (b *writeMem) WriteByte(c byte) error {
+	if b.data == nil {
+		return &Closed{}
+	}
+	b.setSize(b.pos + 1)
+	(*b.data)[b.pos] = c
+	b.pos++
+	return nil
+}
+
+func (b *writeMem) ReadFrom(f io.Reader) (int64, error) {
+	if b.data == nil {
+		return 0, &Closed{}
+	}
+	var (
+		c   int64
+		n   int
+		err error
+	)
+	buf := make([]byte, 1024)
+	for {
+		n, err = f.Read(buf)
+		if n > 0 {
+			c += int64(n)
+			b.setSize(b.pos + n)
+			copy((*b.data)[b.pos:], buf[:n])
+		}
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			break
+		}
+	}
+	return c, err
 }
 
 func (b *writeMem) Seek(offset int64, whence int) (int64, error) {
@@ -132,4 +194,21 @@ func (b *writeMem) Seek(offset int64, whence int) (int64, error) {
 func (b *writeMem) Close() error {
 	b.data = nil
 	return nil
+}
+
+func (b *writeMem) setSize(end int) {
+	if end > len(*b.data) {
+		if end < cap(*b.data) {
+			*b.data = (*b.data)[:end]
+		} else {
+			var newData []byte
+			if len(*b.data) < 512 {
+				newData = make([]byte, end, end<<1)
+			} else {
+				newData = make([]byte, end, end+(end>>2))
+			}
+			copy(newData, *b.data)
+			*b.data = newData
+		}
+	}
 }
