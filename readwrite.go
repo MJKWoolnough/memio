ed3dc782141257fae078bf0d1135b0731f2d4f0f
Michael Woolnough <michael.woolnough@gmail.com>
2015-07-27 18:36:44 +0100

    Added mem ReadWrite type

---
+++ readwrite.go
@@ -0,0 +1,60 @@
+package memio
+
+import "io"
+
+type ReadWriteMem struct {
+	WriteMem
+}
+
+// OpenMem uses a byte slice for reading and writing. Implements io.Reader,
+// io.Writer, io.Seeker, io.ReaderAt, io.ByteReader, io.WriterTo, io.WriterAt,
+// io.ByteWriter and io.ReaderFrom.
+func OpenMem(data *[]byte) *ReadWriteMem {
+	return &ReadWriteMem{WriteMem{data, 0}}
+}
+
+// Read is an implementation of the io.Reader interface
+func (b *ReadWriteMem) Read(p []byte) (int, error) {
+	if b.data == nil {
+		return 0, ErrClosed
+	} else if b.pos >= len(*b.data) {
+		return 0, io.EOF
+	}
+	n := copy(p, (*b.data)[b.pos:])
+	b.pos += n
+	return n, nil
+}
+
+// ReadByte is an implementation of the io.ByteReader interface
+func (b *ReadWriteMem) ReadByte() (byte, error) {
+	if b.data == nil {
+		return 0, ErrClosed
+	} else if b.pos >= len(*b.data) {
+		return 0, io.EOF
+	}
+	c := (*b.data)[b.pos]
+	b.pos++
+	return c, nil
+}
+
+// ReadAt is an implementation of the io.ReaderAt interface
+func (b *ReadWriteMem) ReadAt(p []byte, off int64) (int, error) {
+	if b.data == nil {
+		return 0, ErrClosed
+	} else if off >= int64(len(*b.data)) {
+		return 0, io.EOF
+	}
+	return copy(p, (*b.data)[off:]), nil
+}
+
+// WriteTo is an implementation of the io.WriterTo interface
+func (b *ReadWriteMem) WriteTo(f io.Writer) (int64, error) {
+	if b.data == nil {
+		return 0, ErrClosed
+	} else if b.pos >= len(*b.data) {
+		return 0, io.EOF
+	}
+	n, err := f.Write((*b.data)[b.pos:])
+	b.pos = len(*b.data)
+	return int64(n), err
+}
---
+++ readwrite_test.go
@@ -0,0 +1,208 @@
+package memio
+
+import (
+	"io"
+	"testing"
+)
+
+var (
+	_ io.Reader     = new(ReadMem)
+	_ io.Closer     = new(ReadMem)
+	_ io.Seeker     = new(ReadMem)
+	_ io.WriterTo   = new(ReadMem)
+	_ io.ByteReader = new(ReadMem)
+	_ io.ReaderAt   = new(ReadMem)
+
+	_ io.Writer     = new(WriteMem)
+	_ io.Closer     = new(WriteMem)
+	_ io.Seeker     = new(WriteMem)
+	_ io.ReaderFrom = new(WriteMem)
+	_ io.ByteWriter = new(WriteMem)
+	_ io.WriterAt   = new(WriteMem)
+)
+
+func TestReadWriteRead(t *testing.T) {
+	data := []byte("Hello, World!")
+	reader := OpenMem(&data)
+	toRead := make([]byte, 5)
+	if n, err := reader.Read(toRead); n != 5 {
+		t.Errorf("expecting to read 5 bytes, read %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(toRead) != "Hello" {
+		t.Errorf("expecting %q, got %q", "Hello", string(toRead))
+		return
+	}
+	if n, err := reader.Read(toRead); n != 5 {
+		t.Errorf("expecting to read 5 bytes, read %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(toRead) != ", Wor" {
+		t.Errorf("expecting %q, got %q", ", Wor", string(toRead))
+		return
+	}
+	if n, err := reader.Read(toRead); n != 3 {
+		t.Errorf("expecting to read 3 bytes, read %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(toRead) != "ld!or" {
+		t.Errorf("expecting %q, got %q", "ld!or", string(toRead))
+		return
+	}
+	if n, err := reader.Read(toRead); n != 0 {
+		t.Errorf("expecting to read 0 bytes, read %d", n)
+		return
+	} else if err != io.EOF {
+		t.Errorf("expecting EOF")
+	}
+	if pos, err := reader.Seek(2, 0); pos != 2 {
+		t.Errorf("expected to be at postion 2, got %d", pos)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if n, err := reader.Read(toRead); n != 5 {
+		t.Errorf("expecting to read 5 bytes, read %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(toRead) != "llo, " {
+		t.Errorf("expecting %q, got %q", "llo, ", string(toRead))
+		return
+	}
+	if pos, err := reader.Seek(2, 1); pos != 9 {
+		t.Errorf("expected to be at postion 9, got %d", pos)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if n, err := reader.Read(toRead); n != 4 {
+		t.Errorf("expecting to read 4 bytes, read %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(toRead) != "rld! " {
+		t.Errorf("expecting %q, got %q", "rld! ", string(toRead))
+		return
+	}
+	if pos, err := reader.Seek(-6, 2); pos != 7 {
+		t.Errorf("expected to be at postion 7, got %d", pos)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if n, err := reader.Read(toRead); n != 5 {
+		t.Errorf("expecting to read 5 bytes, read %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(toRead) != "World" {
+		t.Errorf("expecting %q, got %q", "World", string(toRead))
+		return
+	}
+	if _, err := reader.Seek(1, 0); err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	}
+	reader.Close()
+}
+
+func TestReadWriteWrite(t *testing.T) {
+	data := []byte("Beep")
+	writer := OpenMem(&data)
+	if n, err := writer.Write([]byte("J")); n != 1 {
+		t.Errorf("expecting to write 1 byte, wrote %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(data) != "Jeep" {
+		t.Errorf("expecting %q, got %q", "Jeep", string(data))
+		return
+	}
+	if n, err := writer.Write([]byte("ohn")); n != 3 {
+		t.Errorf("expecting to write 3 bytes, wrote %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(data) != "John" {
+		t.Errorf("expecting %q, got %q", "John", string(data))
+		return
+	}
+	if n, err := writer.Write([]byte("ny")); n != 2 {
+		t.Errorf("expecting to write 2 bytes, wrote %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(data) != "Johnny" {
+		t.Errorf("expecting %q, got %q", "Johnny", string(data))
+		return
+	}
+	if pos, err := writer.Seek(0, 0); pos != 0 {
+		t.Errorf("expected to be at postion 0, got %d", pos)
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if n, err := writer.Write([]byte("Edmund")); n != 6 {
+		t.Errorf("expecting to write 6 bytes, wrote %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(data) != "Edmund" {
+		t.Errorf("expecting %q, got %q", "Edmund", string(data))
+		return
+	}
+	if pos, err := writer.Seek(-4, 2); pos != 2 {
+		t.Errorf("expected to be at postion 0, got %d", pos)
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if n, err := writer.Write([]byte("war")); n != 3 {
+		t.Errorf("expecting to write 3 bytes, wrote %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(data) != "Edward" {
+		t.Errorf("expecting %q, got %q", "Edward", string(data))
+		return
+	}
+	if pos, err := writer.Seek(1, 1); pos != 6 {
+		t.Errorf("expected to be at postion 6, got %d", pos)
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if n, err := writer.Write([]byte("o")); n != 1 {
+		t.Errorf("expecting to write 1 bytes, wrote %d", n)
+		return
+	} else if err != nil {
+		t.Errorf("got error: %q", err.Error())
+		return
+	} else if string(data) != "Edwardo" {
+		t.Errorf("expecting %q, got %q", "Edwardo", string(data))
+		return
+	}
+	writer.Close()
+	_, err := writer.Seek(0, 0)
+	if err != ErrClosed {
+		t.Errorf("expecting close error")
+		return
+	}
+	_, err = writer.Write([]byte("Beep"))
+	if err != ErrClosed {
+		t.Errorf("expecting close error")
+		return
+	}
+}
