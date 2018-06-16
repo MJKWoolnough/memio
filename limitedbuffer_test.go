fa8683992878b45b8787ab658d1b3430cd6c3c29
Michael Woolnough <michael.woolnough@gmail.com>
2018-06-16 17:46:27 +0100

    added ReadFrom tests

--- limitedbuffer_test.go
+++ limitedbuffer_test.go
@@ -1,14 +1,16 @@
 package memio
 
 import (
+	"bytes"
 	"io"
 	"testing"
 )
 
 var (
-	_ io.Reader   = &LimitedBuffer{}
-	_ io.Writer   = &LimitedBuffer{}
-	_ io.WriterTo = &LimitedBuffer{}
+	_ io.Reader     = &LimitedBuffer{}
+	_ io.Writer     = &LimitedBuffer{}
+	_ io.WriterTo   = &LimitedBuffer{}
+	_ io.ReaderFrom = &LimitedBuffer{}
 )
 
 func TestLimitedBufferWrite(t *testing.T) {
@@ -38,6 +40,60 @@ func TestLimitedBufferWrite(t *testing.T) {
 		t.Errorf("expecting io.ErrShortBuffer, got: %s", err)
 		return
 	} else if n != 0 {
-		t.Errorf("expecting to write 0 bytes, read %d", n)
+		t.Errorf("expecting to write 0 bytes, wrote %d", n)
+	}
+}
+
+type byteReader byte
+
+func (b byteReader) Read(p []byte) (int, error) {
+	if int(b) < len(p) {
+		p = p[:b]
+	}
+	for i := byte(0); i < byte(b); i++ {
+		p[i] = i
+	}
+	return len(p), nil
+}
+
+func TestLimitedBufferReadFrom(t *testing.T) {
+	l := make(LimitedBuffer, 0, 30)
+	n, err := l.ReadFrom(byteReader(1))
+	if n != 30 {
+		t.Errorf("expecting to read 30 bytes, read %d", n)
+	} else if err != nil {
+		t.Errorf("go unexpected error: %s", err)
+	} else if !bytes.Equal(l, bytes.Repeat([]byte{0}, 30)) {
+		t.Errorf("expecting 30 0's, got %v", l)
+	}
+
+	l = l[:0]
+	n, err = l.ReadFrom(byteReader(2))
+	if n != 30 {
+		t.Errorf("expecting to read 30 bytes, read %d", n)
+	} else if err != nil {
+		t.Errorf("go unexpected error: %s", err)
+	} else if !bytes.Equal(l, bytes.Repeat([]byte{0, 1}, 15)) {
+		t.Errorf("expecting 15 [0, 1]'s, got %v", l)
+	}
+
+	l = l[:0]
+	n, err = l.ReadFrom(byteReader(3))
+	if n != 30 {
+		t.Errorf("expecting to read 30 bytes, read %d", n)
+	} else if err != nil {
+		t.Errorf("go unexpected error: %s", err)
+	} else if !bytes.Equal(l, bytes.Repeat([]byte{0, 1, 2}, 10)) {
+		t.Errorf("expecting 10 [0, 1, 2]'s, got %v", l)
+	}
+
+	l = l[:0]
+	n, err = l.ReadFrom(io.LimitReader(byteReader(4), 20))
+	if n != 20 {
+		t.Errorf("expecting to read 20 bytes, read %d", n)
+	} else if err != nil {
+		t.Errorf("go unexpected error: %s", err)
+	} else if !bytes.Equal(l, bytes.Repeat([]byte{0, 1, 2, 3}, 5)) {
+		t.Errorf("expecting 5 [0, 1, 2, 3]'s, got %v", l)
 	}
 }
